902bcbfeeb79f5c12859967aae8f7ec74c812d9d
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-03-23 20:50:29 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Button } from "@codepiercer/svelte-tailwind"
+</script>
+
+<div class="flex h-screen w-full max-w-none flex-col items-center justify-center gap-2">
+  <h1>Welcome to p0</h1>
+  <p>Arthur Casagrande's Graphical Method</p>
+
+  <Button>Click me</Button>
+</div>
